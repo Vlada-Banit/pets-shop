70eb718ca4b350fd44a8a78f5a7c40be1cbcc8f3
Vlada-Banit <bani0101@mail.ru>
2024-05-12 22:02:51 +0300

    first exфьm

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const items = [{
         tags: ["cat", "dog"],
         price: 500,
         img: "./img/1.jpeg",
+        rating: 5.0,
     },
     {
         title: "Игрушка лабиринт",
@@ -11,6 +12,7 @@ const items = [{
         tags: ["cat", "dog"],
         price: 900,
         img: "./img/2.jpeg",
+        rating: 4.2,
     },
     {
         title: "Игрушка для котят",
@@ -18,6 +20,7 @@ const items = [{
         tags: ["cat"],
         price: 300,
         img: "./img/3.jpeg",
+        rating: 4.5,
     },
     {
         title: "Миска «Котик»",
@@ -25,6 +28,7 @@ const items = [{
         tags: ["cat", "dog"],
         price: 660,
         img: "./img/4.jpeg",
+        rating: 4.0,
     },
     {
         title: "Лоток розовый",
@@ -32,6 +36,7 @@ const items = [{
         tags: ["cat"],
         price: 400,
         img: "./img/5.jpeg",
+        rating: 4.0,
     },
     {
         title: "Сухой корм для кошек",
@@ -39,6 +44,7 @@ const items = [{
         tags: ["cat"],
         price: 200,
         img: "./img/6.jpeg",
+        rating: 4.6,
     },
     {
         title: "Сухой корм для собак",
@@ -46,6 +52,7 @@ const items = [{
         tags: ["dog"],
         price: 300,
         img: "./img/7.jpeg",
+        rating: 4.7,
     },
     {
         title: "Игрушка для собак",
@@ -53,6 +60,7 @@ const items = [{
         tags: ["dog"],
         price: 500,
         img: "./img/8.jpeg",
+        rating: 4.9,
     },
     {
         title: "Лежанка",
@@ -60,6 +68,7 @@ const items = [{
         tags: ["cat", "dog"],
         price: 1500,
         img: "./img/9.jpeg",
+        rating: 5.0,
     },
     {
         title: "Поилка для собак",
@@ -67,6 +76,7 @@ const items = [{
         tags: ["dog"],
         price: 800,
         img: "./img/10.jpeg",
+        rating: 2.0,
     },
     {
         title: "Переноска",
@@ -74,6 +84,7 @@ const items = [{
         tags: ["cat", "dog"],
         price: 3500,
         img: "./img/11.jpeg",
+        rating: 4.4,
     },
     {
         title: "Поводок для собак",
@@ -81,15 +92,39 @@ const items = [{
         tags: ["dog"],
         price: 800,
         img: "./img/12.jpeg",
+        rating: 4.0,
     },
 ];
 
-
+let currentState = [...items];
 const shopCards = document.querySelector('#shop-items');
 const cardTemplate = document.querySelector('#item-template');
+const nothingFound = document.querySelector('#nothing-found');
+
+function renderItems(arr) {
+    nothingFound.textContent = "";
+    shopCards.innerHTML = "";
+    arr.forEach((item) => {
+        shopCards.append(createNewCard(item));
+    });
+    if (!arr.length) {
+        nothingFound.textContent = "Ничего не найдено";
+    }
+}
+
+function sortByAlphabet(a, b) {
+    if (a.title > b.title) {
+        return 1;
+    }
+    if (a.title < b.title) {
+        return -1;
+    }
+    return 0;
+}
+renderItems(currentState.sort((a, b) => sortByAlphabet(a, b)));
 
 function createNewCard(cardItem) {
-    const { title, description, tags, price, img } = cardItem;
+    const { title, description, tags, price, img, rating } = cardItem;
     const card = cardTemplate.content.cloneNode(true);
 
     card.querySelector('h1').textContent = title;
@@ -97,7 +132,12 @@ function createNewCard(cardItem) {
     card.querySelector('span').textContent = price;
     card.querySelector('img').src = img;
 
-
+    const ratingContainer = card.querySelector(".rating");
+    for (let i = 0; i < rating; i++) {
+        const star = document.createElement("i");
+        star.classList.add("fa", "fa-star");
+        ratingContainer.append(star);
+    }
 
     const tagPlace = card.querySelector('.tags');
 
@@ -117,3 +157,50 @@ function showCards() {
         shopCards.append(createNewCard(card))
     })
 }
+
+const sortControl = document.querySelector("#sort");
+sortControl.addEventListener("change", (event) => {
+    const selectedOption = event.target.value;
+    switch (selectedOption) {
+        case "expensive":
+            {
+                currentState.sort((a, b) => b.price - a.price);
+                break;
+            }
+        case "cheap":
+            {
+                currentState.sort((a, b) => a.price - b.price);
+                break;
+            }
+        case "rating":
+            {
+                currentState.sort((a, b) => b.rating - a.rating);
+                break;
+            }
+        case "alphabet":
+            {
+                currentState.sort((a, b) => sortByAlphabet(a, b));
+                break;
+            }
+    }
+    renderItems(currentState);
+});
+
+const searchInput = document.querySelector("#search-input");
+const searchButton = document.querySelector("#search-btn");
+
+function applySearch() {
+    const searchString = searchInput.value.trim().toLowerCase();
+
+    currentState = items.filter((el) =>
+        el.title.toLowerCase().includes(searchString)
+    );
+    currentState.sort((a, b) => sortByAlphabet(a, b));
+    renderItems(currentState);
+    sortControl.selectedIndex = 0;
+}
+
+
+searchButton.addEventListener("click", applySearch);
+
+searchInput.addEventListener("search", applySearch);
